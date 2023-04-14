275b27c59107c4bc8487c4eb09992a859c2be242
crocux <mih.avram@gmail.com>
2023-04-14 16:50:58 +0300

    remove duplicated code

--- script.js
+++ script.js
@@ -2,58 +2,49 @@
 var searchBox = document.getElementById("searchBox")
 var searchIcon = document.getElementById("searchIcon")
 
+async function superFetch(link){
+    const response = await fetch(link)
+    const data = await response.json()
+    return data;
+}
 
 async function getCoords(cityName){
     const geoLocatie = "https://geocoding-api.open-meteo.com/v1/search?language=en&count=1&format=json&name=";
     var coordLink = geoLocatie + cityName
-    const response = await fetch(coordLink)
-    const data = await response.json()
+    const data = await superFetch(coordLink)
     //console.log(data);
     //console.log(data.results[0].latitude, data.results[0].longitude);
     var returnPackage = {
         latitude: data.results[0].latitude,
         longitude: data.results[0].longitude,
+        judet: data.results[0].admin1,
+        tara: data.results[0].country,
+        elevation: data.results[0].elevation,
+        nume: data.results[0].nume
     }
-    return returnPackage;
+    console.log(returnPackage);
+    return returnPackage;    
 }
-//console.log(getCoords("Bacau"))
 
 async function getWeather (coord){
     const apiUrl = "https://api.open-meteo.com/v1/forecast?current_weather=true&timezone=auto"
 var lat = "&latitude=" + coord.latitude
 var lng = "&longitude=" + coord.longitude
 var weatherLink = apiUrl + lat + lng
-const response = await fetch(weatherLink)
-const data = await response.json()
-console.log(data);
+const data = await superFetch(weatherLink)
+var returnPackage = {
+    isDay: data.current_weather.is_day,
+    temperature:data.current_weather.temperature,
+    time:data.current_weather.time,
+    weatherCode:data.current_weather.weathercode,
+    windSpeed:data.current_weather.windspeed,
+    windDirection:data.current_weather.winddirection
 }
+console.log(returnPackage);
+}
+
 async function testerSync(){
-    let coord = await getCoords("Bacau")
+    let coord = await getCoords("Miercurea")
     getWeather(coord);
 }
 testerSync()
-
-
-
-        
-
-//         //document.querySelector(".temp").innerHTML = Math.round(data.hourly.temperature) + "°C";
-//current_weather:
-// is_day
-// : 
-// 1
-// temperature
-// : 
-// 18.5
-// time
-// : 
-// "2023-04-14T16:00"
-// weathercode
-// : 
-// 2
-// winddirection
-// : 
-// 136
-// windspeed
-// : 
-// 18.6
